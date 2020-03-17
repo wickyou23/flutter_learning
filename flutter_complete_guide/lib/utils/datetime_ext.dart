2ef50315075a8fd8da439e6c387cfbaa5a649b32
Thang Phung <lienthang9121991@gmail.com>
2020-03-17 23:56:40 +0700

    no message

--- flutter_complete_guide/lib/utils/datetime_ext.dart
+++ flutter_complete_guide/lib/utils/datetime_ext.dart
@@ -6,8 +6,17 @@ extension DateTimeExt on DateTime {
     return format.format(this);
   }
 
+  DateTime startOfDate() {
+    return DateTime(this.year, this.month, this.day);
+  }
+
+  DateTime endOfDate() {
+    return DateTime(this.year, this.month, this.day, 23, 59, 59);
+  }
+
+  //Start at Monday
   static List<DateTime> dateInWeekByDate(DateTime date) {
-    final now = DateTime.now();
+    final now = DateTime.now().startOfDate();
     final weekDay = date.weekday;
     final result = <DateTime>[]; 
     for (var i = 1; i <= 7; i++) {
