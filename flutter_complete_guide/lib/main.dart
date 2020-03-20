497c16a6e88152fd23ed7948e73a150307523a11
wickyou23 <phunglienhuy1810@gmail.com>
2020-03-20 12:55:29 +0700

    Setup project

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 // import 'package:flutter/services.dart';
-import 'transaction_home_page.dart';
+import 'home_page.dart';
 
 void main() {
   // Lock Orientation
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
               ),
         ),
       ),
-      home: MyHomeApp(),
+      home: HomePage(),
     );
   }
 }
